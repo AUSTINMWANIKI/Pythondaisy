8b499324ea7ca51e992ff6a02ddc11752491d4a6
AUSTINMWANIKI <austinmwaniki112@gmail.com>
2025-06-16 04:31:59 -0400

    final commit

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+
+number = 45 #Integer
+second = 34.98 #float
+greeting = "Good Morning" #String
+isPythonIntresting = True #Boolean
+
+#Data Structures
+fruits = ["apple", "banana", "cherry"] #This is a list
+cars = ("Jeep", "BMW", "Toyota","Mitsubishi") #Tuple
+countries={"Kenya", "Canada","Hungary"} #Set
+student={
+    "firstname" :"Victory",
+    "lastname"  :"Smith",
+    "age": 21,
+    "nationality": "Kenyan",
+}#Dictionary - Key-value pair
+
+print(number)
+print(isPythonIntresting)
+print(fruits)
+print(cars)
+print(countries)
+print(student)
+print(student["nationality"])
+
+#Typecasting - Converting one datatype to another
+print(float(number))
+print(int(second))
